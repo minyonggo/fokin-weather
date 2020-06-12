96b043e32c890ccf0538c8d552e74849ab46b82a
Minyonggo#2020.06.12 <minyonggo731@gmail.com>
2020-06-12 22:30:05 +0900

    #1.0 Layouts with Flexbox in React Native

--- App.js
+++ App.js
@@ -4,16 +4,22 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.subView} />
+      <View style={styles.mainView} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  subView: {
+    flex: 1,
+    backgroundColor: "#B6DAE4"
+  },
+  mainView : {
+    flex: 4,
+    backgroundColor: "#E6F0F8"
+  }
 });
