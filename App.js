da5be74dd1dfcacbec17c1c7060a33b4813254cf
Minyonggo#2020.06.12 <minyonggo731@gmail.com>
2020-06-12 23:45:19 +0900

    #1.2 Getting the Location

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from "expo-location";
 import Loading from './Loading';
 
-export default function App() {
-  return (
-    <Loading />
-  );
+export default class App extends React.Component {
+  state = {
+    latitude : 0,
+    longitude : 0,
+  }
+  
+  getGeolocation = async() => {
+    const currentLocation = await Location.getCurrentPositionAsync(); 
+    this.setState({
+      latitude : currentLocation.coords.latitude,
+      longitude : currentLocation.coords.longitude,
+    });
+
+    const {longitude, latitude} = this.state;
+    console.log(latitude, longitude);
+  }
+
+  componentDidMount() {
+    this.getGeolocation();
+  }
+
+  render() {
+    return (
+      <Loading />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
